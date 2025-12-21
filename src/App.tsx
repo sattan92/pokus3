982c83a7e150bc6a1ee896f87895215235a35bb7
sattan92 <134283868+sattan92@users.noreply.github.com>
2025-12-21 13:08:58 +0100

    vylepsenie

--- src/App.tsx
+++ src/App.tsx
@@ -10,36 +10,40 @@ function App() {
   return (
     <>
       <div className="all w-screen h-screen">
-        <div className='h-min text-xl lg:text-[35px] md:text-[25px] p-[2vw] grid grid-cols-3 grid-rows-1 justify-center'>
+        <div className='text-purple-700 font-bold  h-min text-xl lg:text-[35px] md:text-[25px] p-[2vw] grid grid-cols-3 grid-rows-1 justify-center'>
 
-          <div id='hodiny' className='text-end content-center md:p-[3vw] p-5 order-3 justify-self-end h-full bg-white/20 backdrop-blur-md border border-white/20 shadow-xl rounded-xl w-min'>
+          <div id='hodiny' className='text-start content-center md:p-[3vw] p-5 order-3 justify-self-end h-full bg-white/20 backdrop-blur-md border border-white/20 shadow-xl rounded-xl w-min'>
             <Clock></Clock>
           </div>
           <h2 className="md:p-[4vw] content-center h-full w-min bg-white/20 backdrop-blur-md border border-white/20 shadow-xl rounded-xl p-5">Login</h2>
           <h2 className='md:p-[4vw] content-center h-full w-min bg-white/20 backdrop-blur-md border border-white/20 shadow-xl rounded-xl p-5'>Welcome: <UserName></UserName></h2>
           <div className='absolute -z-10 inset-0' style={{ width: '100vw', height: '100vh', position: 'absolute' }}>
             <LiquidChrome
-              baseColor={[0.1, 0, 0.1]}
-              speed={0.3}
-              amplitude={0.4}
+              baseColor={[0.05, 0, 0.1]}
+              speed={0.2}
+              amplitude={0.2}
               interactive={false} />
           </div>
         </div>
         <div className="md:p-5 md:mt-[1vw] mt-[-1vw] mr-[2vw] ml-[2vw] line bg-white/20 backdrop-blur-md border border-white/20 shadow-xl rounded-xl p-1.5"></div>
-        <div className="md:gap-[4vw] md:mt-[2vw] mt-[-1vw] text-xl lg:text-[25px] md:text-[18px] p-[2vw] custom-spotlight-card grid grid-cols-2 gap-[2vw]">
-          <SpotlightCard className=" " spotlightColor="rgba(208, 67, 255, 0.78)">
-            <h1 className='text-xl lg:text-[35px] md:text-[25px] pb-2'>Features</h1>
-            <div className='ml-[2vw]'>
-            <ol className='grid grid-rows-5 h-min gap-2'>
-              <li>Killaura</li>
-              <li>No-fall</li>
-              <li>Anti-Knockback</li>
-              <li>Flight</li>
-              <li>Free cam</li>
-            </ol></div>
-          </SpotlightCard><SpotlightCard className="custom-spotlight-card" spotlightColor="rgba(0, 229, 255, 0.2)">
-  // Content goes here
+        <div className="text-purple-700 font-bold md:gap-[vw] md:mt-[2vw] mt-[-1vw] text-xl lg:text-[25px] md:text-[18px] p-[2vw] custom-spotlight-card grid grid-cols-2 md:grid-cols-3 gap-[2vw]">
+          <SpotlightCard className="" spotlightColor="rgba(108, 67, 255, 0.59)">
+            <h1 className='text-2xl lg:text-[45px] md:text-[35px] pb-2'>Features</h1>
+            <div className='ml-[1vw]'>
+              <ol className='grid grid-rows-5 h-min text-lg lg:text-[25px] md:text-[18px]'>
+                <li>Killaura</li>
+                <li>No-fall</li>
+                <li>Anti-Knockback</li>
+                <li>Flight</li>
+                <li>Free cam</li>
+              </ol></div>
+          </SpotlightCard><SpotlightCard className="custom-spotlight-card" spotlightColor="rgba(108, 67, 255, 0.59)">
+            <h1 className='text-2xl lg:text-[45px] md:text-[35px] pb-2'>Reviews</h1>
+          </SpotlightCard>
+          <SpotlightCard className="custom-spotlight-card col-span-2 md:col-span-1" spotlightColor="rgba(108, 67, 255, 0.59)">
+            <h1 className='text-2xl lg:text-[45px] md:text-[35px] pb-2 '>Price</h1>
           </SpotlightCard></div>
+          
       </div>
     </>
   )
